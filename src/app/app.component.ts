3a6749b03f006e2937ca6dd27325fcd1bb109ccc
Victor <overhandvictor@gmail.com>
2023-03-07 17:46:45 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+interface User {
+  name: string,
+  age: number
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'test-todo';
+export class AppComponent implements OnInit {
+  tasks!: User[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<User[]>('http://localhost:3000/data').subscribe(data => {
+      this.tasks = data;
+    });
+  }
+
+  addNewData() {
+    this.http.put<User[]>('http://localhost:3000/data', { name: 'Alice', age: 20 })
+      .subscribe(data => {
+        this.tasks = data;
+      });
+  }
+
 }
